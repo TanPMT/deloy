15adac06b736b2d01730f6d13edbec3b0429c224
Phạm Minh Tân <22521310@gm.uit.edu.vn>
2024-05-19 19:50:27 +0700

    fix 2

--- FOF/settings.py
+++ FOF/settings.py
@@ -154,12 +154,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIR=[
+    BASE_DIR / 'static',
     os.path.join(BASE_DIR, 'STATIC')
 ]
 
