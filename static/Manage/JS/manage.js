3da0da938fd288798a32082caad2a9e7c87d0fb7
Phạm Minh Tân <115931062+TanPMT@users.noreply.github.com>
2024-05-20 23:56:07 +0700

    Update manage.js

--- static/Manage/JS/manage.js
+++ static/Manage/JS/manage.js
@@ -50,7 +50,7 @@ function formatDate(date) {
         var pH_num = document.getElementById('pH'); 
         var doAm = document.getElementById('doam'); 
   
-        var address = landInfo.land_pos;
+        var address = landInfo.position;
         var geocodingUrl = `https://rsapi.goong.io/geocode?address=${encodeURIComponent(address)}&api_key=irESJaYhu9GSYh2g8waOxsDolPKwMzkFcGKNGxNy`;
         // Điền dữ liệu vào phần tử map    
         fetch(geocodingUrl)
